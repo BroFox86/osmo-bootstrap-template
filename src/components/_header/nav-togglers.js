ac53db3fe2103481d4c3ed243237fa84e7c9eb11
Daur Gamisonia <daurgam@gmail.com>
2018-06-06 15:58:33 +0300

    Updates

--- src/components/_header/nav-togglers.js
+++
@@ -1,9 +0,0 @@
-$("[data-toggle='menuToggler']").click(function() {
-  $(this).toggleClass("is-expanded");
-  $("[data-target='navList']").toggleClass("is-visible");
-});
-
-$("[data-toggle='navListNested']").click(function() {
-  $(this).addClass("is-expanded");
-  $("[data-target='navListNested']").slideToggle(400);
-});
