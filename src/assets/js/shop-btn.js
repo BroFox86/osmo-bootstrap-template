6ca39def096b7c4023fb198e6c23d97cdb34d8a6
Daur Gamisonia <daurgam@gmail.com>
2018-11-02 16:41:42 +0300

    Fix button appearance

--- src/assets/js/shop-btn.js
+++ src/assets/js/shop-btn.js
@@ -5,7 +5,7 @@
 $(window).on("DOMContentLoaded resize", function() {
   if (window.matchMedia("(max-width: 768px)").matches) {
     setTimeout(function() {
-      $(".menu-shop").addClass("is-visible");
+      $(".menu .shop-btn").addClass("is-visible");
     }, 500);
   }
 });
